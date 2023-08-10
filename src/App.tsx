a73def126bdf3a6867bca2bbe4779f982e065e37
Sandip Thapa <sandipthapa386@gmail.com>
2023-08-10 20:14:38 +0545

    feat(pages): Added home and team pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Team from "./pages/Team";
 
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/my-team" element={<Team />} />
+      </Routes>
     </div>
   );
 }
